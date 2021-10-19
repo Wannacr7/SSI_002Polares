b57412e3ed38285f0baf3a9ed7d0bfd590c579cd
Wannacr7 <88002544+Wannacr7@users.noreply.github.com>
2021-10-19 09:18:59 -0500

    Part one

--- Assets/Scripts/LookAt.cs
+++ Assets/Scripts/LookAt.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class LookAt : MonoBehaviour
 {
+
+    Vector2 velocity;
+    Vector2 aceleration;
+    
+    [Header("Activa o desactiva el movimiento")][SerializeField]private bool isMoving = false;
+
     // Update is called once per frame
     void Update()
     {
         
-        var mousePosition = GetWorldMousePosition();
+        Vector2 mousePosition = GetWorldMousePosition();
+        Vector2 thisPosition = this.transform.position;
+
         Debug.DrawLine(Vector3.zero, mousePosition, Color.white);
-        Look(mousePosition);
+
+        if(!isMoving)Look(mousePosition);
+        if (isMoving) Move(mousePosition, thisPosition);
+       
+
+        
+        
     }
 
     private void Look(Vector2 targetPosition)
@@ -20,11 +34,25 @@ public class LookAt : MonoBehaviour
         float radians = Mathf.Atan2(forward.y, forward.x);
         RotateZ(radians);
     }
+
+    private void Move(Vector2 _mousePosition, Vector2 _thisPosition)
+    {
+        aceleration = _mousePosition - _thisPosition;
+        velocity = velocity + aceleration * Time.deltaTime;
+
+        Look(_mousePosition + velocity);
+
+        Vector3 finalPosition = new Vector3(velocity.x, velocity.y, 0);
+        this.transform.position += finalPosition * Time.deltaTime;
+
+    }
+
     private Vector4 GetWorldMousePosition()
     {
         Camera camera = Camera.main;
         Vector3 screenPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, camera.nearClipPlane);
         Vector4 worldPos = Camera.main.ScreenToWorldPoint(screenPos);
+        worldPos.z = 0;
         return worldPos;
     }
 
